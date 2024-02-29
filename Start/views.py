d194cbd17c07b5eecc6da44c574036456a63276a
kavingojulius <kavingojulius1@gmail.com>
2024-02-29 20:39:35 +0300

    made some changes

--- Start/views.py
+++ Start/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import *
+
 # Create your views here.
 
 def Home(request):
@@ -7,10 +9,14 @@ def Home(request):
     # if request.method == 'GET':
     work = ImagesWork.objects.all()
     video_work = VideosWork.objects.all()
-    
+    paginator = Paginator(video_work, 2)
+    page_number = request.GET.get("page")
+    pagee = paginator.get_page(page_number)
+
     context = {
         'work' : work,
-        'video_work' : video_work
+        'video_work' : video_work,
+        'pagee' : pagee
     }
 
     
